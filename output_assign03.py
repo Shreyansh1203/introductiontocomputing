1d2ff21ab6366878e4ba4d4305157a31f15c6fec
Shreyansh1203 <96442666+Shreyansh1203@users.noreply.github.com>
2022-02-14 21:18:39 +0530

    Add files via upload

---
+++ output_assign03.py
@@ -0,0 +1,76 @@
+#####  ques 1 #####
+
+enter string: hello hello world hello world 
+{'hello': 3, 'world': 2}
+
+#####  ques 2 #####
+enter day: 14
+enter month: 02
+enter year: 2022
+Next Date is :  15 / 2 / 2022
+
+##### ques 3  ######
+enter elements of list seperated by ',' 
+5,3,6
+[(5, 25), (3, 9), (6, 36)]
+
+##### ques 4 ######
+enter grade points : 9
+Your Grade is 'A' and Excellent performance
+
+###### ques 5 ######
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+##### ques 6 #####
+Enter SID: 21103076
+Enter Name: shreyansh
+Do you want to enter another student entry(Y or N):y
+Enter SID: 999999
+Enter Name: abhi
+Do you want to enter another student entry(Y or N):y
+Enter SID: 211030769
+Enter Name: bholu
+Do you want to enter another student entry(Y or N):n
+<a> {21103076: 'shreyansh', 999999: 'abhi', 211030769: 'bholu'}
+<b> {999999: 'abhi', 211030769: 'bholu', 21103076: 'shreyansh'}
+<c> {999999: 'abhi', 21103076: 'shreyansh', 211030769: 'bholu'}
+Search Name with SID: 21103076
+<d> shreyansh
+
+
+#### ques 7 ######
+ENTER THE NUMBER OF TERMS IN THE SERIES: 8
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+AVERAGE OF RESULTANT FIBONACCI SERIES IS 4.125
+
+
+##### ques 8 ######
+<a> {1, 3, 5, 6, 8}
+<b> {3, 6, 8, 9, 13, 17}
+<c> {1, 2, 4, 5}
+<d> {6, 7, 8, 9, 10}
+<e> {10, 7}
+
+
+
+
+
+
+
+
+
+
+
