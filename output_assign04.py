f8d01800a83b95506d33042ea2cb3d969933dbcf
Shreyansh1203 <96442666+Shreyansh1203@users.noreply.github.com>
2022-02-27 00:24:50 +0530

    Add files via upload

---
+++ output_assign04.py
@@ -0,0 +1,64 @@
+#########  ques 1 ###########
+
+move 1st disk from source to destination  
+move 2 th disk from  source to helper     
+move 1st disk from destination to helper  
+move 3 th disk from  source to destination
+move 1st disk from helper to source       
+move 2 th disk from  helper to destination
+move 1st disk from source to destination 
+
+########  ques 2 #############
+
+Enter the size of pascals triangle 7
+by loop   
+        1 
+       1 1
+      1 2 1
+     1 3 3 1
+    1 4 6 4 1
+   1 5 10 10 5 1
+  1 6 15 20 15 6 1
+
+
+
+using recursion
+            1
+          1   1   
+        1   2   1
+      1   3   3   1
+    1   4   6   4   1
+  1   5   10   10   5   1
+1   6   15   20   15   6   1
+
+############  ques 3  #############
+
+Enter 2 numbers: 6 8
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+<b> The quotient is zero
+<c> Filtered out numbers that are greater than 4 are : [10, 11, 5, 11, 6, 12]
+<d> Set: {5, 6, 10, 11, 12}
+<e> Immutable set: frozenset({5, 6, 10, 11, 12})
+<f> Hash value of the max value from the above set: 12
+
+############  ques 4  ############
+
+Object created
+The name of the student it shreyansh and SID is 21103076.
+Object destroyed
+
+########## ques 5 ###################
+
+<a> The updated salary of Mehak is 70000
+<b>Record of Viren deleted
+
+########### ques 6 ####################
+
+Enter the word: apple
+Enter a new meaningful word using the exact same letters to test your friendship: lappe
+Does the word makes sense?(y/Y or n/N )y
+The friends pass the friendship test
+
+
